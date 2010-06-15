eac0da38aa61467a66b8e68124d3afcbfa4df405
Tim James <cubed2d@nekocake.com>
2010-06-15 02:34:31 +0100

    Vector3.Multiply.cs  -- Added multiplication methods and operators. Vector3.Dot.cs  -- Added dot product methods.
    Almost got all the operators XNA has copied.

---
+++ Crimson.Maths/Vector3/Vector3.Dot.cs
@@ -0,0 +1,16 @@
+﻿namespace Crimson.Maths
+{
+    public partial struct Vector3
+    {
+
+        public static float Dot(Vector3 a, Vector3 b)
+        {
+            return (((a.X*b.X) + (a.Y*b.Y)) + (a.Z*b.Z));
+        }
+
+        public static void Dot(ref Vector3 vector1, ref Vector3 vector2, out float result)
+        {
+            result = ((vector1.X * vector2.X) + (vector1.Y * vector2.Y)) + (vector1.Z * vector2.Z);
+        }
+    }
+}
---
+++ Crimson.Maths/Vector3/Vector3.Multiply.cs
@@ -0,0 +1,75 @@
+﻿namespace Crimson.Maths
+{
+    public partial struct Vector3
+    {
+        public static Vector3 operator *(Vector3 a, Vector3 b)
+        {
+            return new Vector3
+            {
+                X = a.X * b.X,
+                Y = a.Y * b.Y,
+                Z = a.Z * b.Z
+            };
+        }
+
+        public static Vector3 operator *(Vector3 a, float scaler)
+        {
+            return new Vector3
+            {
+                X = a.X * scaler,
+                Y = a.Y * scaler,
+                Z = a.Z * scaler
+            };
+        }
+
+        public static Vector3 operator *(float scaler, Vector3 a)
+        {
+            return new Vector3
+            {
+                X = a.X * scaler,
+                Y = a.Y * scaler,
+                Z = a.Z * scaler
+            };
+        }
+
+        public static Vector3 Multiply(Vector3 a, Vector3 b)
+        {
+            return new Vector3
+            {
+                X = a.X * b.X,
+                Y = a.Y * b.Y,
+                Z = a.Z * b.Z
+            };
+        }
+
+        public static void Multiply(ref Vector3 a, ref Vector3 b, out Vector3 @return)
+        {
+            @return = new Vector3
+            {
+                X = a.X * b.X,
+                Y = a.Y * b.Y,
+                Z = a.Z * b.Z
+            };
+        }
+
+        public static Vector3 Multiply(Vector3 a, float scaler)
+        {
+            return new Vector3
+            {
+                X = a.X * scaler,
+                Y = a.Y * scaler,
+                Z = a.Z * scaler
+            };
+        }
+
+        public static void Multiply(ref Vector3 a, float scaler, out Vector3 @result)
+        {
+            @result = new Vector3
+            {
+                X = a.X * scaler,
+                Y = a.Y * scaler,
+                Z = a.Z * scaler
+            };
+        }
+    }
+}
