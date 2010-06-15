ad97a62685e6692af37bd64427a466e40a03dd4d
Tim James <cubed2d@nekocake.com>
2010-06-15 22:31:47 +0100

    Vector3.Clamp.cs  -- adds vector clamping methods
    Vector3.Negate.cs
     -- adds negation methods and operator

---
+++ Crimson.Maths/Vector3/Vector3.Clamp.cs
@@ -0,0 +1,49 @@
+﻿namespace Crimson.Maths
+{
+    public partial struct Vector3
+    {
+        public static Vector3 Clamp(Vector3 value1, Vector3 min, Vector3 max)
+        {
+            float x = value1.X;
+            x = (x > max.X) ? max.X : x;
+            x = (x < min.X) ? min.X : x;
+
+            float y = value1.Y;
+            y = (y > max.Y) ? max.Y : y;
+            y = (y < min.Y) ? min.Y : y;
+
+            float z = value1.Z;
+            z = (z > max.Z) ? max.Z : z;
+            z = (z < min.Z) ? min.Z : z;
+
+            return new Vector3
+            {
+                X = x,
+                Y = y,
+                Z = z
+            };
+        }
+
+        public static void Clamp(ref Vector3 value1, ref Vector3 min, ref Vector3 max, out Vector3 result)
+        {
+            float x = value1.X;
+            x = (x > max.X) ? max.X : x;
+            x = (x < min.X) ? min.X : x;
+
+            float y = value1.Y;
+            y = (y > max.Y) ? max.Y : y;
+            y = (y < min.Y) ? min.Y : y;
+
+            float z = value1.Z;
+            z = (z > max.Z) ? max.Z : z;
+            z = (z < min.Z) ? min.Z : z;
+
+            result = new Vector3
+            {
+                X = x,
+                Y = y,
+                Z = z
+            };
+        }
+    }
+}
---
+++ Crimson.Maths/Vector3/Vector3.Negate.cs
@@ -0,0 +1,35 @@
+﻿namespace Crimson.Maths
+{
+    public partial struct Vector3
+    {
+        public static Vector3 operator -(Vector3 vector3)
+        {
+            return new Vector3
+            {
+                X = -vector3.X,
+                Y = -vector3.Y,
+                Z = -vector3.Z
+            };
+        }
+
+        public static Vector3 Negate(Vector3 vector3)
+        {
+            return new Vector3
+            {
+                X = -vector3.X,
+                Y = -vector3.Y,
+                Z = -vector3.Z
+            };
+        }
+
+        public static void Negate(ref Vector3 vector3, out Vector3 result)
+        {
+            result = new Vector3
+            {
+                X = -vector3.X,
+                Y = -vector3.Y,
+                Z = -vector3.Z
+            };
+        }
+    }
+}
